30685d7900b030b45fd25cba023004067ef5fa61
sushma83 <lakumarapusushma007@gmail.com>
2022-05-30 16:19:05 +0530

    initial commit

---
+++ src/com/cts/Main.java
@@ -0,0 +1,10 @@
+package com.cts;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    System.out.println("Welcome to session");
+	    System.out.println("Hope you are able to follow?");
+	}
+
+}
